05d5577c3afb6bf23e47ef85a8aaad47508cbe4c
Mihail Danilovich <danilovich36misha@gmail.com>
2025-05-15 14:43:05 +0300

    feat: setup prisma with postgresql

---
+++ prisma/seed.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      email: 'test@gmail.com',
+      name: 'Test',
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
